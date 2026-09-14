d2c17d9b4a9484ffefa60b124160dbe5f5744d2c
Mateus Leite <mateus.leite@clouddog.com.br>
2026-09-14 12:50:02 -0300

    feat: realizando alterações no main.go

--- main.go
+++ main.go
@@ -18,18 +18,22 @@ var r *router.Router
 func init() {
 	r = router.New()
 
+	// Autenticação
 	r.Handle("POST", "/auth/login", auth.HandleLogin)
 	r.Handle("POST", "/auth/registrar", auth.HandleRegistrar)
 
+	// Matrículas
 	r.Handle("GET", "/admin/students", matricula.HandleListar)
 	r.Handle("POST", "/admin/students", matricula.HandleProvisionar)
 	r.Handle("DELETE", "/admin/students", matricula.HandleRemover)
 
+	// Programas
 	r.Handle("POST", "/programas", programa.HandleCriar)
 	r.Handle("GET", "/programas", programa.HandleListar)
 	r.Handle("PUT", "/programas/:programaId", programa.HandleAtualizar)
 	r.Handle("DELETE", "/programas/:programaId", programa.HandleDeletar)
 
+	// Projetos
 	r.Handle("POST", "/programas/:programaId/projetos", projeto.HandleCriar)
 	r.Handle("GET", "/programas/:programaId/projetos", projeto.HandleListarPorPrograma)
 	r.Handle("PUT", "/projetos/:projetoId", projeto.HandleAtualizar)
@@ -38,10 +42,9 @@ func init() {
 	r.Handle("DELETE", "/projetos/:projetoId/orientador", projeto.HandleRemoverOrientador)
 	r.Handle("PUT", "/projetos/:projetoId/integrantes", projeto.HandleAdicionarIntegrante)
 	r.Handle("DELETE", "/projetos/:projetoId/integrantes", projeto.HandleRemoverIntegrante)
-
-	
 }
 
+// Handler para API Gateway
 func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return r.Dispatch(ctx, req)
 }
